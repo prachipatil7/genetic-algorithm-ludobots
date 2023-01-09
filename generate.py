78f443e436ea8bc7bc3bfa4df63e712f9c164164
prachipatil7 <62350419+prachipatil7@users.noreply.github.com>
2023-01-08 18:56:45 -0600

    finished steps 1-20 of part e

--- generate.py
+++ generate.py
@@ -1,18 +1,24 @@
 import pyrosim.pyrosim as pyrosim
 
-pyrosim.Start_SDF("boxes.sdf")
+def create_world():
+    pyrosim.Start_SDF("world.sdf")
 
-for row in range(5):
-    for col in range(5):
-        length, width, height = 1, 1, 1
-        xpos, ypos, zpos = row, col, 0.5
-        for i in range(10):
-            pyrosim.Send_Cube(name=f"Box{i}", 
-                            pos=[xpos, ypos, zpos] , 
-                            size=[length, width, height])
-            length *= 0.9
-            width *= 0.9
-            zpos += height/2
-            height *= 0.9
-            zpos += height/2
-pyrosim.End()
+    length, width, height = 1, 1, 1
+    xpos, ypos, zpos = -3, 3, 0.5
+    pyrosim.Send_Cube(name=f"Box", 
+                    pos=[xpos, ypos, zpos] , 
+                    size=[length, width, height])
+
+    pyrosim.End()
+
+def create_robot():
+    pyrosim.Start_URDF("body.urdf")
+    length, width, height = 1, 1, 1
+    xpos, ypos, zpos = 0, 0, 0.5
+    pyrosim.Send_Cube(name=f"Torso", 
+                    pos=[xpos, ypos, zpos] , 
+                    size=[length, width, height])
+    pyrosim.End()
+
+create_world()
+create_robot()
