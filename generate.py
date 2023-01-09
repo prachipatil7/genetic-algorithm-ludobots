3b40f137faa70981a01071d4d0547db7b224b560
prachipatil7 <62350419+prachipatil7@users.noreply.github.com>
2023-01-08 18:47:25 -0600

    finished 25 towers world

--- generate.py
+++ generate.py
@@ -1,16 +1,18 @@
 import pyrosim.pyrosim as pyrosim
 
-length, width, height = 1, 1, 1
-xpos, ypos, zpos = 0, 0, 0.5
-
 pyrosim.Start_SDF("boxes.sdf")
-for i in range(10):
-    pyrosim.Send_Cube(name=f"Box{i}", 
-                      pos=[xpos, ypos, zpos] , 
-                      size=[length, width, height])
-    length *= 0.9
-    width *= 0.9
-    zpos += height/2
-    height *= 0.9
-    zpos += height/2
+
+for row in range(5):
+    for col in range(5):
+        length, width, height = 1, 1, 1
+        xpos, ypos, zpos = row, col, 0.5
+        for i in range(10):
+            pyrosim.Send_Cube(name=f"Box{i}", 
+                            pos=[xpos, ypos, zpos] , 
+                            size=[length, width, height])
+            length *= 0.9
+            width *= 0.9
+            zpos += height/2
+            height *= 0.9
+            zpos += height/2
 pyrosim.End()
