de5747d01a996b807202ae7b9e3ed130b0a0e15e
prachipatil7 <62350419+prachipatil7@users.noreply.github.com>
2023-01-08 18:38:06 -0600

    pyramid world done

--- generate.py
+++ generate.py
@@ -1,8 +1,16 @@
 import pyrosim.pyrosim as pyrosim
 
-length, width, height = 1, 2, 3
+length, width, height = 1, 1, 1
+xpos, ypos, zpos = 0, 0, 0.5
 
-pyrosim.Start_SDF("box.sdf")
-pyrosim.Send_Cube(name="Box", pos=[0,0,0.5] , size=[length, width, height])
-
+pyrosim.Start_SDF("boxes.sdf")
+for i in range(10):
+    pyrosim.Send_Cube(name=f"Box{i}", 
+                      pos=[xpos, ypos, zpos] , 
+                      size=[length, width, height])
+    length *= 0.9
+    width *= 0.9
+    zpos += height/2
+    height *= 0.9
+    zpos += height/2
 pyrosim.End()
