104300d23e0692651cdefb8b8cd2e6951162463c
prachipatil7 <62350419+prachipatil7@users.noreply.github.com>
2023-01-15 14:00:55 -0600

    finished sensors

--- generate.py
+++ generate.py
@@ -13,11 +13,31 @@ def create_world():
 
 def create_robot():
     pyrosim.Start_URDF("body.urdf")
-    length, width, height = 1, 1, 1
-    xpos, ypos, zpos = 0, 0, 0.5
+    cube = [1, 1, 1]
     pyrosim.Send_Cube(name=f"Torso", 
-                    pos=[xpos, ypos, zpos] , 
-                    size=[length, width, height])
+                    pos=[0, 0, 1.5] , 
+                    size=cube)
+
+    pyrosim.Send_Joint(name = "Torso_FrontLeg",
+                       parent= "Torso" , 
+                       child = "FrontLeg" , 
+                       type = "revolute", 
+                       position = [0.5,0,1])
+
+    pyrosim.Send_Cube(name=f"FrontLeg", 
+                pos= [0.5, 0, -0.5], 
+                size=cube)
+
+    pyrosim.Send_Joint(name = "Torso_BackLeg",
+                   parent= "Torso" , 
+                   child = "BackLeg" , 
+                   type = "revolute", 
+                   position = [-0.5,0,1])
+
+    pyrosim.Send_Cube(name=f"BackLeg", 
+            pos= [-0.5, 0, -0.5], 
+            size=cube)
+    
     pyrosim.End()
 
 create_world()
--- simulate.py
+++ simulate.py
@@ -1,6 +1,9 @@
 import pybullet as p
 import pybullet_data
+import pyrosim.pyrosim as pyrosim
 import time
+import numpy as np
+import matplotlib.pyplot as plt
 
 physicsClient = p.connect(p.GUI)
 p.setAdditionalSearchPath(pybullet_data.getDataPath())
@@ -8,12 +11,26 @@ p.setAdditionalSearchPath(pybullet_data.getDataPath())
 p.setGravity(0,0,-9.8)
 planeId = p.loadURDF("plane.urdf")
 robotId = p.loadURDF("body.urdf")
+pyrosim.Prepare_To_Simulate(robotId)
 
 p.loadSDF("world.sdf")
+backLegSensorValues = np.zeros(1000)
+frontLegSensorValues = np.zeros(1000)
 for i in range(1000):
     p.stepSimulation()
+    backLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("BackLeg")
+    frontLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("FrontLeg")
     time.sleep(1/2000)
-    print(i)
+np.save('data/backLegSensorValues.npy', backLegSensorValues)
+backLegSensorValues = np.load('data/backLegSensorValues.npy')
+
+np.save('data/frontLegSensorValues.npy', frontLegSensorValues)
+frontLegSensorValues = np.load('data/frontLegSensorValues.npy')
+
+plt.plot(backLegSensorValues, label='Back Leg', linewidth=1.5)
+plt.plot(frontLegSensorValues, label='Front Leg')
+plt.legend(loc='upper right')
+plt.show()
 
 p.disconnect()
 
